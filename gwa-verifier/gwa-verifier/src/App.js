a5cfdd3c0a5b0247adbd429347dfe309d025af6b
christianolo <cvolo@up.edu.ph>
2022-04-30 00:12:59 +0700

    feat: removed the Default component since it is a useless component. Added the authentication for login.

--- gwa-verifier/gwa-verifier/src/App.js
+++ gwa-verifier/gwa-verifier/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Default from './components/Default';
+import Login  from './components/Login';
 import Home from './components/Home';
 import Add_Student from './components/Add_Student_Page'
 
@@ -7,7 +7,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Default />} />
+        <Route path='/' element={<Login />} />
         <Route path='/home' element={<Home />} />
         <Route path='/add-student' element={<Add_Student/>}/>
       </Routes>
--- gwa-verifier/gwa-verifier/src/components/Login.js
+++ gwa-verifier/gwa-verifier/src/components/Login.js
@@ -1,61 +1,62 @@
-import React from 'react';
+import useStore from './hooks/authHook'
 import UPLB from '../images/uplb.png'
 import '../css/login.css'
-import { Navigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
-class Login extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={
-            // loginAttempts: 0,
-            redirect: false,
-        }
-        this.login=this.login.bind(this);
-    }
+// changed to function to use hooks
+const Login = () => {
 
-    login(e){
-            // uncomment when integrated with backend
-        // fetch('http:localhost:3001/api/0.1/login' ,{
-        //     method: POST, 
-        //     body: {
-        //           username: document.getElementById('username').value,
-        //           password: document.getElementById('password').value,
-        //     }
-        //  })
-        //  .then(response => response.json())
-        //  .then(body => {
-        //     // validation code
-        // })
+    const { setUser, setIsAuthenticated } = useStore();     // from zustand store
+
+    const navigate = useNavigate();     // hook for navigation
+    
+    // handles login action and 
+    const login = (e) => {
+        e.preventDefault();
         
-        // placeholder action for now
-        this.setState({ redirect: true });
-        e.preventdefault();
+        const credentials = {
+            email: document.getElementById('username').value,
+            password: document.getElementById('password').value
+        }
+
+        fetch('http://localhost:3001/api/0.1/auth' ,{
+                method:'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(credentials)
+            }
+        )
+         .then(response => response.json())
+         .then(body => {
+            if(!body.success) alert(body.message);
+            else{
+                setUser(body.user);
+                setIsAuthenticated(body.success);
+                navigate('/home');
+            }
+        })
     }
 
-    render(){
-        if (this.state.redirect) return <Navigate to={ '/home' } />
-        return(
-            <div className = {'login'}> 
-            <div className= {'login-header'}>
-                <img src = {UPLB} className="uplb-logo" alt="uplb logo"/>
-                <h3 className="web-name">University of the Philippines Los Baños</h3>
-                    
-            </div>
-            <div className={'login-body'}>
-                <form>
-                    <fieldset>
-                        Username: <input type="text" id="username" 
-                        placeholder="enter your username..." 
-                        /><br />
-                        Password: <input type="password" id="password"
-                        placeholder = "enter your password..."/>
-                    </fieldset>
-                    <button onClick = { this.login }>login</button>
-                </form>
-            </div>
-        </div> 
-        )
-    }
+    return(
+        <div className = {'login'}> 
+        <div className= {'login-header'}>
+            <img src = {UPLB} className="uplb-logo" alt="uplb logo"/>
+            <h3 className="web-name">University of the Philippines Los Baños</h3>
+                
+        </div>
+        <div className={'login-body'}>
+            <form>
+                <fieldset>
+                    Username: <input type="text" id="username" 
+                    placeholder="enter your username..." 
+                    /><br />
+                    Password: <input type="password" id="password"
+                    placeholder = "enter your password..."/>
+                </fieldset>
+                <button onClick={login}>Login</button>
+            </form>
+        </div>
+    </div> 
+    )
 }
 
 export default Login;
