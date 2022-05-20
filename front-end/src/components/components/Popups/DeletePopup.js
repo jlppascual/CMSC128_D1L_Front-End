7eb3b55a3f9c481b3016f444810525090792abaa
christianolo <cvolo@up.edu.ph>
2022-05-20 23:56:16 +0700

    feat: added the interface for the reason for deletion of a student.

--- front-end/src/components/components/Popups/DeletePopup.js
+++ front-end/src/components/components/Popups/DeletePopup.js
@@ -1,16 +1,39 @@
 import React, { useState } from 'react';
-import '../../../css/deletepopup.css'
+import '../../../css/deletepopup.css';
+import { notifyError } from './toastNotifUtil';
 
 const DeletePopup=({props})=>{
+    const maxchar = 500;
+    const [char_count, setCharCount] = useState(0)
 
-    const handleButton =async(choice) => {
-        await props.confirmDelete(choice)
+    const handleDetailsChange = (e) =>{
+        setCharCount(e.target.value.trim().length)
+    }
+
+    const handleButton = async(choice) => {
+        let reason = document.getElementById('deletion-details').value.trim();  // this removes trailing and leading spaces
+        console.log(reason)
+        
+        // uncomment if backend logic is already implemented
+        // if(!choice) await props.confirmDelete(choice, reason)
+
+        if(!choice) await props.confirmDelete(choice)
+        else {
+            if (reason === '') notifyError('Please enter a valid reason');
+            else{
+                if (reason.length < 10) notifyError('Reason must not be less than 10 characters');
+                // else await props.confirmDelete(choice, reason);
+                else await props.confirmDelete(choice);
+            }
+        }
     }
     
     return(
         <div className="popup-box">
            
                 <p className='delete-text'>Are you sure you want to delete?</p>
+                <textarea className = 'deletion-details-textarea' id = 'deletion-details' onChange={(e) => handleDetailsChange(e)} maxLength={500} placeholder = "Provide reason for deletion" wrap="hard" ></textarea>
+                <p className = 'details-character-count'>{maxchar - char_count}</p>
                 
                 <div className='buttons'>
                     <button onClick={() => {handleButton(false)}} className = 'cancel-btn'>Cancel</button>
